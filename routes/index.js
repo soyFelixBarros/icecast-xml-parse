b5d001fed90976401b639ad822e8c5dc916eeacb
Felix Barros <soyfelixbarros@gmail.com>
2018-08-24 14:05:35 -0300

    add mounts

--- routes/index.js
+++ routes/index.js
@@ -1,55 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var fs = require('fs');
-var parseString = require('xml2js').parseString;
-var xml2js = require('xml2js');
-const { exec } = require('child_process');
-var icecast = '/etc/icecast2/icecast.xml';
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	fs.readFile('./icecast.xml', 'utf-8', function (err, data) {
-    		if(err) console.log(err);
-    		// we log out the readFile results    
-    		//console.log(data);
-    		// we then pass the data to our method here
-    		parseString(data, function(err, result){
-				if(err) console.log(err);
-				var json = result;
-				if (json.icecast.mount.length > 0) {
-					// json.icecast.mount.forEach(function(value) {
-					// 	console.log(value);
-					// });
-					var keys = Object.keys(json.icecast.mount);
-					// keys.length is 2
-					console.log(keys, keys.length);
-				}
-				console.log("---");
-
-				var mount = {
-					"mount-name": "/example-complex.ogg",
-					"username": "othersource",
-					"password": "hackmemore"
-				};
-
-				// Editando
-				// json.icecast.mount = mount;
-
-			// create a new builder object and then convert
-        		// our json back to xml.
-        		var builder = new xml2js.Builder();
-        		var xml = builder.buildObject(json);
-        
-       			fs.writeFile('./icecast.xml', xml, function(err, data){
-            			if (err) console.log(err);
-						
-						//exec('/etc/init.d/icecast2 reload');
-						
-						console.log("successfully written our update xml to file");
-        		});
-
-    		});
-	});
 	res.render('index', { title: 'Express' });
 });
 
--- routes/mounts.js
+++ routes/mounts.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var fs = require('fs');
+var parseString = require('xml2js').parseString;
+var xml2js = require('xml2js');
+const { exec } = require('child_process');
+var icecast = '/etc/icecast2/icecast.xml';
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('mounts');
@@ -8,8 +14,48 @@ router.get('/', function(req, res, next) {
 
 // POST method route
 router.post('/', function (req, res) {
-    console.log(req.body);
-    res.status(201).json({ status: 'ok' });
+  add_mount(req.body, icecast);
+  res.status(201).json({ status: 'ok' });
 })
+
+function add_mount(mount, icecast) {
+  // Leer el xml de icecast
+  fs.readFile(icecast, 'utf-8', function (err, data) {
+    if(err) console.log(err);
+
+    // luego pasamos los datos a nuestro método aquí
+    parseString(data, function(err, result) {
+      if (err) console.log(err);
+      var json = result;
+
+      // Comprobar si hay motajes y la cantidad
+      if (typeof json.icecast.mount  !== "undefined") {
+        var keys = Object.keys(json.icecast.mount);
+
+        // Agregamos nuevo punto de montaje
+        json.icecast.mount[keys.length] = mount;
+
+        // console.log(keys, keys.length);
+      } else {
+        json.icecast.mount = mount;
+      }
+
+      // Crear un nuevo objeto constructor y luego convertir
+      // nuestro json vuelve a xml.
+      var builder = new xml2js.Builder();
+      var xml = builder.buildObject(json);
+      
+      fs.writeFile(icecast, xml, function(err, data) {
+        if (err) console.log(err);
+
+        // Ejectuamos el comando reload de icecast2 para cargar 
+        // el xml con el nuevo nuevo punto de montaje.
+        exec('/etc/init.d/icecast2 reload');
+
+        // console.log("Actualización xml al archivo :)");
+      });
+    });
+  });
+}
 
 module.exports = router;
