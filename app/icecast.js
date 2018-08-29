58080aa572d6e08085af2ba4085421fc0eb7cc47
Felix Barros <soyfelixbarros@gmail.com>
2018-08-29 14:05:46 -0300

    method delete

--- app/icecast.js
+++ app/icecast.js
@@ -5,8 +5,7 @@ const { exec } = require('child_process');
 
 class Icecast {
     constructor() {
-        // this.path = '/etc/icecast2/icecast.xml';
-        this.path = './icecast.xml';
+        this.path = '/etc/icecast2/icecast.xml';
         this.parseXml();
     }
 
@@ -35,6 +34,20 @@ class Icecast {
         });
     }
 
+    updateXml(obj) {
+        // Crear un nuevo objeto constructor y luego convertir
+        // nuestro json vuelve a xml.
+        var builder = new xml2js.Builder();
+        var xml = builder.buildObject(obj);
+        
+        fs.writeFile(this.path, xml, 'utf8', (err) => {
+            if (err) throw err;
+            // Ejectuamos el comando reload de icecast2 para cargar 
+            // el xml con el nuevo nuevo punto de montaje.
+            exec('/etc/init.d/icecast2 reload');
+        });
+    }
+
     // Buscar un punto de montajes por su key
     find(key) {
         var obj = this.data;
@@ -57,20 +70,27 @@ class Icecast {
         } else {
             obj.icecast.mount = mount;
         }
-        
-        // Crear un nuevo objeto constructor y luego convertir
-        // nuestro json vuelve a xml.
-        var builder = new xml2js.Builder();
-        var xml = builder.buildObject(obj);
-        
-        fs.writeFile(this.path, xml, 'utf8', (err) => {
-            if (err) throw err;
-            // Ejectuamos el comando reload de icecast2 para cargar 
-            // el xml con el nuevo nuevo punto de montaje.
-            exec('/etc/init.d/icecast2 reload');
-        });
-        
-        return key; 
+
+        // Actualizamos el xml con la nueva configuración
+        this.updateXml(obj);
+
+        return key;
+    }
+
+    /**
+     * Eliminar un punto de montaje. 
+     */
+    delete(key) {
+        // Obtenemos el obejcto con toda la configuración
+        var obj = this.data;
+
+        // Eliminamos el punto de montaje
+        delete obj.icecast.mount[key];
+
+        // Guardamos los cambios
+        this.updateXml(obj);
+
+        return typeof obj.icecast.mount[key] == "undefined";
     }
 }
 
--- routes/mounts.js
+++ routes/mounts.js
@@ -7,18 +7,32 @@ router.get('/', function(req, res, next) {
   res.send('mounts');
 });
 
-// POST method route
+// POST mounts
 router.post('/', function (req, res) {
   var icecast = new Icecast;
   var key = icecast.add(req.body);
   res.status(201).json({ key: key });
 });
 
-/* GET mount */
+// // PUT mounts
+// router.put('/', function (req, res) {
+//   var icecast = new Icecast;
+//   var mount = icecast.update(req.body);
+//   res.status(200).json({ data: mount });
+// });
+
+/* GET mounts */
 router.get('/:id', function(req, res, next) {
   var icecast = new Icecast;
   var mount = icecast.find(req.params.id);
   res.status(200).json(mount);
+});
+
+/* DELETE mounts */
+router.delete('/:id', function(req, res, next) {
+  var icecast = new Icecast;
+  var deleted = icecast.delete(req.params.id);
+  res.status(204).json({ deleted: deleted });
 });
 
 
