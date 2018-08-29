2f068e92e1a824b656b58a965d3a904ec25c255a
Felix Barros <soyfelixbarros@gmail.com>
2018-08-28 22:31:24 -0300

    refactor

--- app/icecast.js
+++ app/icecast.js
@@ -17,50 +17,54 @@ class Icecast {
         this._path = value;
     }
 
-    parseXml(path) {
-        const buf =  fs.readFileSync(path, 'utf-8');
-        console.log(typeof buf);
+    get data() {
+        return this._data;
     }
 
-    // add_mount(mount) {
-    //     // Leer el xml de icecast
-    //     fs.readFile(this.path, 'utf-8', function (err, data) {
-    //       if(err) console.log(err);
-      
-    //       // luego pasamos los datos a nuestro método aquí
-    //       parseString(data, function(err, result) {
-    //         if (err) console.log(err);
-    //         var json = result;
-      
-    //         // Comprobar si hay motajes y la cantidad
-    //         if (typeof json.icecast.mount  !== "undefined") {
-    //           var keys = Object.keys(json.icecast.mount);
-      
-    //           // Agregamos nuevo punto de montaje
-    //           json.icecast.mount[keys.length] = mount;
-      
-    //           // console.log(keys, keys.length);
-    //         } else {
-    //           json.icecast.mount = mount;
-    //         }
-      
-    //         // Crear un nuevo objeto constructor y luego convertir
-    //         // nuestro json vuelve a xml.
-    //         var builder = new xml2js.Builder();
-    //         var xml = builder.buildObject(json);
-            
-    //         fs.writeFile(icecast, xml, function(err, data) {
-    //           if (err) console.log(err);
-      
-    //           // Ejectuamos el comando reload de icecast2 para cargar 
-    //           // el xml con el nuevo nuevo punto de montaje.
-    //           exec('/etc/init.d/icecast2 reload');
-      
-    //           // console.log("Actualización xml al archivo :)");
-    //         });
-    //       });
-    //     });
-    //   }
+    set data(value) {
+        this._data  = value;
+    }
+
+    // Parseamos el xml y guardamos la info como un objecto
+    parseXml(path) {
+        var xml = fs.readFileSync(path, 'utf-8');
+
+        parseString(xml, (err, result) => {
+            this.data = result;
+        });
+    }
+
+    find(key) {
+        var obj = this.data;
+        return JSON.stringify(obj.icecast.mount[key]);
+    }
+
+    add(mount) {
+        var obj = this.data;
+        var key = 0;
+        if (typeof obj.icecast.mount !== "undefined") {
+            var keys = Object.keys(obj.icecast.mount);
+            key = keys.length;
+            // Agregamos nuevo punto de montaje
+            obj.icecast.mount[key] = mount;
+        } else {
+            obj.icecast.mount = mount;
+        }
+        
+        // Crear un nuevo objeto constructor y luego convertir
+        // nuestro json vuelve a xml.
+        var builder = new xml2js.Builder();
+        var xml = builder.buildObject(obj);
+        
+        fs.writeFile(this.path, xml, 'utf8', (err) => {
+            if (err) throw err;
+            // Ejectuamos el comando reload de icecast2 para cargar 
+            // el xml con el nuevo nuevo punto de montaje.
+            exec('/etc/init.d/icecast2 reload');
+        });
+        
+        return key; 
+    }
 }
 
 module.exports = Icecast;
--- routes/mounts.js
+++ routes/mounts.js
@@ -2,19 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 var Icecast = require('../app/icecast');
-// var path = '/etc/icecast2/icecast.xml';
-var path = './icecast.xml';
+var path = '/etc/icecast2/icecast.xml';
+// var path = './icecast.xml';
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  var icecast = new Icecast(path);
   res.send('mounts');
 });
 
 // POST method route
 router.post('/', function (req, res) {
   // add_mount(req.body, icecast);
-  res.status(201).json({ status: 'ok' });
+  var icecast = new Icecast(path);
+  var key = icecast.add(req.body);
+  res.status(201).json({ status: 'ok', key: key });
 });
+
+/* GET mount */
+router.get('/:id', function(req, res, next) {
+  var icecast = new Icecast(path);
+  var mount = icecast.find(req.params.id);
+  res.status(200).json(mount);
+});
+
 
 module.exports = router;
